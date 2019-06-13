c64548a1be9f24acce4865052d8acded85a83415
Achmad Rizky Haqiqi <rzkhqq@gmail.com>
2019-06-13 07:35:42 +0200

    Use unity basic call to fetch REST Data

--- Assets/Scripts/ObjectColoring.cs
+++ Assets/Scripts/ObjectColoring.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using SimpleFirebaseUnity;
+using UnityEngine.Networking;
+using System.Collections;
+using MiniJSON;
 
 public class ObjectColoring : MonoBehaviour
 {
@@ -9,59 +11,53 @@ public class ObjectColoring : MonoBehaviour
     public string objectId = "Ob001";
     public string selectedType = "Humidity";
     public TextMesh t;
-    public Firebase firebase;
 
     #region data and firebase
-    public void InitFirebase()
-    {
-        this.firebase = Firebase.CreateNew("https://iotunity.firebaseio.com/");
-        // firebase.OnGetSuccess += GetOKHandler;
-        firebase.OnGetFailed += GetFailHandler;
-    }
 
-    public void GetObjectSensors()
+    IEnumerator FetchObjectSensors()
     {
-        Firebase sensors = firebase.Child("Objects/" + objectId, true);
-        sensors.OnGetSuccess += GetObjectSensorsHandler;
-        sensors.GetValue();
-    }
+        UnityWebRequest www = UnityWebRequest.Get("https://iotunity.firebaseio.com/Objects/" + objectId + ".json");
+        yield return www.SendWebRequest();
 
-    void GetObjectSensorsHandler(Firebase sender, DataSnapshot snapshot)
-    {
-        List<string> keys = snapshot.Keys;
-        if (keys != null)
-            foreach (string key in keys)
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            var dictValue = Json.Deserialize(www.downloadHandler.text) as Dictionary<string, object>;
+
+            foreach (var item in dictValue)
             {
-                GetSensorData(key);
+                Debug.Log(item.Key);
+                StartCoroutine(FetchSensorData(item.Key));
             }
+        }
     }
 
-    public void GetSensorData(string key)
+    IEnumerator FetchSensorData(string key)
     {
-        Firebase sensor = firebase.Child("Sensors/" + key + "/Values", true);
-        sensor.OnGetSuccess += GetSensorDataHandler;
-        sensor.GetValue();
-    }
+        UnityWebRequest www = UnityWebRequest.Get("https://iotunity.firebaseio.com/Sensors/" + key + "/Values.json");
+        yield return www.SendWebRequest();
 
-    void GetSensorDataHandler(Firebase sender, DataSnapshot snapshot)
-    {
-        Dictionary<string, object> dict = snapshot.Value<Dictionary<string, object>>();
-        List<string> keys = snapshot.Keys;
-
-        if (keys != null)
-            foreach (string key in keys)
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            var dictValue = Json.Deserialize(www.downloadHandler.text) as Dictionary<string, object>;
+            foreach (var item in dictValue)
             {
-                if (!valueDict.ContainsKey(key))
+                Debug.Log(item.Key);
+                if (!valueDict.ContainsKey(item.Key))
                 {
-                    valueDict.Add(key, dict[key]);
-                } 
+                    valueDict.Add(item.Key, dictValue[item.Key]);
+                }
             }
+        }
     }
 
-    public void FetchData()
-    {
-        GetObjectSensors();
-    }
     #endregion
 
     public Color GetColor()
@@ -104,7 +100,7 @@ public class ObjectColoring : MonoBehaviour
 
     void Start()
     {
-        InitFirebase();
+        StartCoroutine(FetchObjectSensors());
 
         rend = GetComponent<Renderer>();
         rend.material.color = Color.white;
@@ -118,8 +114,7 @@ public class ObjectColoring : MonoBehaviour
         t.transform.position = gameObject.transform.position;
 
         t.transform.position += new Vector3(0f, 0f, -1f);
-        
-        FetchData();
+
     }
 
     // Update is called once per frame
@@ -127,27 +122,4 @@ public class ObjectColoring : MonoBehaviour
     {
         rend.material.color = GetColor();
     }
-
-    #region handler
-    void GetOKHandler(Firebase sender, DataSnapshot snapshot)
-    {
-        Debug.Log("[OK] Get from key: <" + sender.FullKey + ">");
-        Debug.Log("[OK] Raw Json: " + snapshot.RawJson);
-
-        Dictionary<string, object> dict = snapshot.Value<Dictionary<string, object>>();
-        List<string> keys = snapshot.Keys;
-
-        if (keys != null)
-            foreach (string key in keys)
-            {
-                Debug.Log(key + " = " + dict[key].ToString());
-            }
-    }
-
-    void GetFailHandler(Firebase sender, FirebaseError err)
-    {
-        Debug.Log("[ERR] Get from key: <" + sender.FullKey + ">,  " + err.Message + " (" + (int)err.Status + ")");
-    }
-
-    #endregion
 }
